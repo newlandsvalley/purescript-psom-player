12c616ffade3f0899bee304f4974ba4c8eb6e8ad
John Watson <john.watson@gmx.co.uk>
2017-07-29 08:58:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+purescript-psom-player
+======================
+
+
+This is a purescript-pux module that is a player for [Purescript School of Music](https://github.com/newlandsvalley/purescript-school-of-music) performances.
+
+As with the MIDI player, it is a specialisation of the basic [soundfont-player](https://github.com/newlandsvalley/purescript-soundfont-player). To build a Melody for the player, you need two things - the PSoM Performance and the set of MIDI instruments that are in scope.  Again, the building of the Melody is deferred to the point where the Play button is first pressed.
+
+The calling program can use __SetPerformance__ to re-initialise the player with a new performance and __SetInstrumentMap__ to tell the player the instruments that are in scope.  
+
+to build the module
+-------------------
+
+   bower install
+
+   pulp build
+
+   
+dependencies
+------------
+
+| Module                     | Reference                                                              |
+| -------------------------- | ---------------------------------------------------------------------- |
+| purescript-school-of-Music | https://github.com/newlandsvalley/purescript-school-of-music.git       |
+| purescript-soundfonts      | https://github.com/newlandsvalley/purescript-polyphonic-soundfonts.git |
+| purescript-soundont_player | https://github.com/newlandsvalley/purescript-soundfont_player.git      |
+| purescript-pux             | 9.1.0                                                                  |
---
+++ src/Audio/Euterpea/Player.purs
@@ -0,0 +1,87 @@
+module Audio.Euterpea.Player
+  (State, Event (SetPerformance, SetInstrumentMap), initialState, foldp, view) where
+
+import Prelude ((&&), (==))
+import Audio.BasePlayer as BasePlayer
+import Audio.Euterpea.ToMelody (perf2melody)
+import Data.Euterpea.Instrument (InstrumentMap)
+import Data.Euterpea.Midi.MEvent
+import Data.Function (($), (#))
+import Data.Array (null)
+import Data.Map (empty)
+import Data.Maybe (Maybe(..))
+import Audio.SoundFont (AUDIO)
+import Pux (EffModel, noEffects, mapEffects, mapState)
+import Pux.DOM.HTML (HTML, mapEvent)
+
+
+data Event =
+    SetPerformance Performance       -- we'll set the melody from the Euterpea Performance
+  | SetInstrumentMap InstrumentMap
+  | BasePlayerEvent BasePlayer.Event
+
+type State =
+  { melodySource :: Maybe Performance
+  , instrumentMap :: InstrumentMap
+  , basePlayer :: BasePlayer.State
+  }
+
+initialState :: State
+initialState =
+  { melodySource : Nothing
+  , instrumentMap : empty
+  , basePlayer : BasePlayer.initialState
+  }
+
+foldp :: ∀ fx. Event -> State -> EffModel State Event (au :: AUDIO | fx)
+foldp (SetPerformance performance) state =
+  noEffects $ setPerformance performance state
+foldp (SetInstrumentMap instrumentMap) state =
+    noEffects $ state { instrumentMap = instrumentMap }
+foldp (BasePlayerEvent e) state =
+  let
+    -- establish the melody only when the Play button is first pressed
+    newState =
+      case e of
+        BasePlayer.PlayMelody playbackState ->
+          if (playbackState == BasePlayer.PLAYING) && (null state.basePlayer.melody) then
+            establishMelody state
+          else
+            state
+        _ -> state
+  in
+    delegate e newState
+
+-- | set a PSoM Performance
+setPerformance :: Performance -> State -> State
+setPerformance performance state =
+  let
+    bpState = BasePlayer.setMelody [] state.basePlayer
+  in
+    state { melodySource = Just performance, basePlayer = bpState }
+
+
+-- | delegate to the Base Player
+delegate :: ∀ fx. BasePlayer.Event -> State -> EffModel State Event (au :: AUDIO | fx)
+delegate e state =
+  BasePlayer.foldp e state.basePlayer
+    # mapEffects BasePlayerEvent
+    # mapState \pst -> state { basePlayer = pst }
+
+-- | establish the Base Player melody from thh performance (if we have it)
+establishMelody :: State -> State
+establishMelody state =
+  let
+    melody =
+      case state.melodySource of
+        Just performance ->
+          perf2melody state.instrumentMap performance
+        _ ->
+         []
+    bpState = BasePlayer.setMelody melody state.basePlayer
+  in
+    state { basePlayer = bpState }
+
+view :: State -> HTML Event
+view state =
+  mapEvent BasePlayerEvent $ BasePlayer.view state.basePlayer
